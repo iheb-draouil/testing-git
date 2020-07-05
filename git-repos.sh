7f8df2d93ce849fa726642c751d288a255e926f0
Iheb Draouil <iheb.draouil@outlook.com>
2020-07-05 03:17:31 +0100

    auto-commit

--- git-repos.sh
+++ git-repos.sh
@@ -4,9 +4,9 @@ files=$(find . -name '.git')
 
 for file in $files
 do
-    echo -e "# Current directory: ${file%/*}\n"
+    echo -e "\n# Current directory: ${file%/*}\n"
     git -C "${file%/*}" add .
     git -C "${file%/*}" commit -m "auto-commit"
     git -C "${file%/*}" push
-    echo -e "\n\n\n\n"
+    echo -e "\n\n-------------\n\n"
 done
